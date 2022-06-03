4f464a8357a3a6f90d1e16542f1dbd81e4cd2ef3
talikeb <talikeb20@gmail.com>
2022-06-03 14:39:00 -0400

    test commit (added comments to __init__.py)

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
-import os
-from flask import Flask, render_template, request
+import os  # Import operating system tools
+from flask import Flask, render_template, request  # Import flask library and associated tools
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Loads .env file
 app = Flask(__name__)
 
-
+# Index route
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+# Branch to run site
+if __name__ == "__main__":
+    app.run(debug=True)
