38b251ff76c26014959dffb50fa6ebde2e3424b1
talikeb <talikeb20@gmail.com>
2022-06-04 04:00:35 -0400

    added basic nav bar; created home page foundation

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 import os  # Import operating system tools
-from flask import Flask, render_template, request  # Import flask library and associated tools
+from flask import Flask, render_template, request, url_for  # Import flask library and associated tools
 from dotenv import load_dotenv
+
+# NOTE: url_for is linked to the functions below and returns the corresponding index; preferred over hardcoding links
 
 load_dotenv()  # Loads .env file
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Fix later."
+    return render_template('home.html', title="Home", url=os.getenv("URL"))
 
 @app.route('/Victor')
 def victor_page():
