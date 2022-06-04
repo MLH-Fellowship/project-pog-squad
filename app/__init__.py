f01fa782d76617d59ab3b4e0896bf05149a104d8
talikeb <talikeb20@gmail.com>
2022-06-04 01:40:30 -0400

    created placeholders for Job Experience and Education sections

--- app/__init__.py
+++ app/__init__.py
@@ -5,25 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()  # Loads .env file
 app = Flask(__name__)
 
-"""
-# Index route
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))  # Page content
-"""
-
 @app.route('/')
 def home():
     return "Fix later."
 
 @app.route('/Victor')
 def victor_page():
-    return render_template('victor.html', title="Victor B.", url=os.getenv("URL"))
+    v_titles = ["PE Fellow", "Software Engineer", "Intern"]
+    v_dates = ["May 2022 - Present", "January 2017 - December 2018", "May 2016 - August 2016"]
+    v_locations = ["MLH", "Blue People", "Google"]
+    return render_template('victor.html', title="Victor B.", url=os.getenv("URL"),
+        titles=v_titles, dates=v_dates, locations=v_locations)
 
 @app.route('/Talike')
 def talike_page():
-    return render_template('talike.html', title="Talike B.", url=os.getenv("URL"))
-
+    # NOTE: Using dictionary for first two lists did not work
+    t_titles = ["PE Fellow", "Intern", "Seasonal Aide"]
+    t_dates = ["May 2022 - Present", "January 2022", "June 2021 - August 2021"]
+    t_locations = ["MLH", "UNICC", "NYC Parks"]
+    return render_template('talike.html', title="Talike B.", url=os.getenv("URL"),
+        titles=t_titles, dates=t_dates, locations=t_locations)
 
 # Branch to run site
 if __name__ == "__main__":
