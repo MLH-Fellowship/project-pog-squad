aee33cbe3003befdb80e38349ac151c00b311c3a
cheveuxdelin <victorba2120@gmail.com>
2022-06-05 16:58:45 -0500

    Fixes for routing and styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from asyncio import constants
 import os  # Import operating system tools
 from flask import Flask, render_template, request, url_for  # Import flask library and associated tools
 from dotenv import load_dotenv
@@ -7,11 +8,27 @@ from dotenv import load_dotenv
 load_dotenv()  # Loads .env file
 app = Flask(__name__)
 
-# Index route
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
-# Branch to run site
+@app.route("/victor")
+@app.route("/victor/")
+def victor():
+    return render_template("/victor/home.html", url=os.getenv("URL"))
+
+@app.route("/victor/hobbies")
+@app.route("/victor/hobbies/")
+def victor_hobbies():
+    return render_template("/victor/hobbies.html", url=os.getenv("URL"))
+
+
+@app.route("/talike")
+@app.route("/talike/")
+def talike():
+    return render_template("/talike/home.html", url=os.getenv("URL"))
+
+@app.route("/talike/hobbies")
+@app.route("/talike/hobbies/")
+def talike_hobbies():
+    return render_template("/talike/hobbies.html", url=os.getenv("URL"))
+
 if __name__ == "__main__":
     app.run(debug=True)
