e32f15e64ca664d2f9304f422c5290cce36618ee
cheveuxdelin <victorba2120@gmail.com>
2022-06-05 00:32:43 -0500

    revert for fixes

--- app/__init__.py
+++ app/__init__.py
@@ -7,26 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()  # Loads .env file
 app = Flask(__name__)
 
+# Index route
 @app.route('/')
-def home():
-    return render_template('home.html', title="Home", url=os.getenv("URL"))
-
-@app.route('/Victor')
-def victor_page():
-    v_titles = ["PE Fellow", "Software Engineer", "Intern"]
-    v_dates = ["May 2022 - Present", "January 2017 - December 2018", "May 2016 - August 2016"]
-    v_locations = ["MLH", "Blue People", "Google"]
-    return render_template('victor.html', title="Victor B.", url=os.getenv("URL"),
-        titles=v_titles, dates=v_dates, locations=v_locations)
-
-@app.route('/Talike')
-def talike_page():
-    # NOTE: Using dictionary for first two lists did not work
-    t_titles = ["PE Fellow", "Intern", "Seasonal Aide"]
-    t_dates = ["May 2022 - Present", "January 2022", "June 2021 - August 2021"]
-    t_locations = ["MLH", "UNICC", "NYC Parks"]
-    return render_template('talike.html', title="Talike B.", url=os.getenv("URL"),
-        titles=t_titles, dates=t_dates, locations=t_locations)
+def index():
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
 # Branch to run site
 if __name__ == "__main__":
