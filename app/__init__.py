4ad6cf0a41b24da84754a285669a6d608b07fc71
talikeb <talikeb20@gmail.com>
2022-06-19 03:04:37 -0400

    changed image paths

--- app/__init__.py
+++ app/__init__.py
@@ -12,23 +12,23 @@ app = Flask(__name__)
 @app.route("/victor")
 @app.route("/victor/")
 def victor():
-    return render_template("/victor/home.html", img="victor.jpeg",title="Victor", url=os.getenv("URL"))
+    return render_template("/victor/home.html", img="Victor.jpeg",title="Victor", url=os.getenv("URL"))
 
 @app.route("/victor/hobbies")
 @app.route("/victor/hobbies/")
 def victor_hobbies():
-    return render_template("/victor/hobbies.html", img="victor.jpeg",title="Victor", url=os.getenv("URL"))
+    return render_template("/victor/hobbies.html", img="Victor.jpeg",title="Victor", url=os.getenv("URL"))
 
 
 @app.route("/talike")
 @app.route("/talike/")
 def talike():
-    return render_template("/talike/home.html", img="talike.jpg",title="Talike", url=os.getenv("URL"))
+    return render_template("/talike/home.html", img="Talike.jpg",title="Talike", url=os.getenv("URL"))
 
 @app.route("/talike/hobbies")
 @app.route("/talike/hobbies/")
 def talike_hobbies():
-    return render_template("/talike/hobbies.html", img="talike.jpg", title="Talike", url=os.getenv("URL"))
+    return render_template("/talike/hobbies.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
 
 if __name__ == "__main__":
     app.run(debug=True)
