3656899914b028fa2170516a9198e6d3c880ac38
Victor Bustillos <68461123+cheveuxdelin@users.noreply.github.com>
2022-06-22 14:58:46 -0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,10 +3,7 @@
 tmux kill-server
 cd /root/project-pog-squad
 git fetch && git reset origin/main --hard > /dev/null
-source env/bin/activate
-pip3 install flask > /dev/null
-pip3 install python-dotenv > /dev/null
+source python3-virtualenv/bin/activate
+pip3 install -r requirements.txt
 tmux new -d -s portfolio
 tmux send -t portfolio "flask run --host=0.0.0.0" Enter
-deactivate
-cd ..
