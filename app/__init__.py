07b75aa7b09a8428afe0fdec94776f8e80184526
talikeb <talikeb20@gmail.com>
2022-06-22 02:25:27 -0400

    going solo

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,20 @@
 from asyncio import constants
 import os  # Import operating system tools
 from flask import Flask, render_template, request, url_for  # Import flask library and associated tools
-from dotenv import load_dotenv
+from dotenv import load_dotenv # ?
 
 # NOTE: url_for is linked to the functions below and returns the corresponding index; preferred over hardcoding links
 
 load_dotenv()  # Loads .env file
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
 
-@app.route("/victor")
-@app.route("/victor/")
-def victor():
-    return render_template("/victor/home.html", img="Victor.jpeg",title="Victor", url=os.getenv("URL"))
-
-@app.route("/victor/hobbies")
-@app.route("/victor/hobbies/")
-def victor_hobbies():
-    return render_template("/victor/hobbies.html", img="Victor.jpeg",title="Victor", url=os.getenv("URL"))
-
-
-@app.route("/talike")
-@app.route("/talike/")
-def talike():
-    return render_template("/talike/home.html", img="Talike.jpg",title="Talike", url=os.getenv("URL"))
-
-@app.route("/talike/hobbies")
-@app.route("/talike/hobbies/")
-def talike_hobbies():
-    return render_template("/talike/hobbies.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
+@app.route("/hobbies/")
+def hobbies():
+    return render_template("hobbies.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
 
 if __name__ == "__main__":
     app.run(debug=True)
