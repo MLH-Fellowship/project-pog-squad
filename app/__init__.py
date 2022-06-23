a6bbf3f77b246e09056a95ea2a8adfe8b9269469
talikeb <talikeb20@gmail.com>
2022-06-22 22:18:34 -0400

    added missing character

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
-from asyncio import constants
+from asyncio import constants # ?
 import os  # Import operating system tools
 from flask import Flask, render_template, request, url_for  # Import flask library and associated tools
 from dotenv import load_dotenv # ?
 
 # NOTE: url_for is linked to the functions below and returns the corresponding index; preferred over hardcoding links
+
+"""
+TODO:
+- merge Hobbies page into Home page
+- create three main pages to navigate to: MLH Fellowship, About Me, Contact
+"""
 
 load_dotenv() # Loads .env file
 app = Flask(__name__)
