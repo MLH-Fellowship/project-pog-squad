5273072709ae1a0cbe97cae32857b0670a407208
talikeb <talikeb20@gmail.com>
2022-06-22 22:44:58 -0400

    removed Hobbies page; added new pages

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,14 @@ app = Flask(__name__)
 def home():
     return render_template("home.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
 
+@app.route("/about-me/")
+def about_me():
+    return render_template("about_me.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
+
 @app.route("/hobbies/")
 def hobbies():
     return render_template("hobbies.html", img="Talike.jpg", title="Talike", url=os.getenv("URL"))
