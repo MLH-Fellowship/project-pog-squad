ae04089fa86a7a879ebcb675a96449d123c9d9d2
cheveuxdelin <victorba2120@gmail.com>
2022-06-26 01:22:22 +0000

    script update

--- a.sh
+++ a.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-tmux kill-server
-cd /root/project-pog-squad
-git fetch && git reset origin/main --hard > /dev/null
+cd /root/MLH-portfolio
+git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
-tmux new -d -s portfolio
-tmux send -t portfolio "flask run --host=0.0.0.0" Enter
+systemctl daemon-reload
+systemctl restart myportfolio
