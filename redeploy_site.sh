a64b1cdaebc7e20a3e3282cd14ee2fcc1881a871
Talike <talikeb20@gmail.com>
2022-06-28 00:02:51 -0400

    revised redeploy_site.sh

--- redeploy_site.sh
+++ redeploy_site.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-tmux kill-session -t portfolio
 cd MLH-portfolio-site
 git fetch && git reset origin/main --hard > /dev/null
 source env/bin/activate
 pip3 install -r requirements.txt > /dev/null
-tmux new -d -s portfolio
-tmux send -t portfolio "flask run --host=0.0.0.0" Enter
+systemctl daemon-reload
+systemctl restart myportfolio
 deactivate
 cd ..
