d82d2bf2275e6629dd8d96bf3746f8f3f02f89a2
Star Xie <59263550+xiestar0310@users.noreply.github.com>
2022-07-03 23:32:51 -0400

    added tests from week 5

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,21 @@ from peewee import *
 
 from playhouse.shortcuts import model_to_dict
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os. getenv( "MYSQL _USER" ), password=os. getenv ( "MYSQL_PASSWORD" ) , host=os.getenv( "MYSQL_HOST" ), port=3306)
+load_dotenv()  # Loads .env file
+app = Flask(__name__)
 
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared',uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv ("MYSQL_PASSWORD") ,
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
+
+print(mydb)
 class TimelinePost(Model):
     name = CharField()
     email = CharField()
@@ -21,9 +34,6 @@ class TimelinePost(Model):
 
 mydb.connect()
 mydb.create_tables([TimelinePost])
-
-load_dotenv("../.env")  # Loads .env file
-app = Flask(__name__)
 
 @app.route("/home")
 def home():
@@ -43,18 +53,29 @@ if __name__ == "__main__":
 
 @app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
+    # Check if name is valid
+    if "name" not in request.form:
+        return "Invalid name", 400
+
     name = request.form["name"]
     email = request.form["email"]
     content = request.form["content"]
+    # check for empty content
+    if content == "":
+        return "Invalid content", 400
+    # check for malformed email
+    elif "@" not in email:
+        return "Invalid email", 400
+
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
-    return model_to_dict(timeline_post)    
+    return model_to_dict(timeline_post)
 
 @app.route("/api/timeline_post", methods=["GET"])
 def get_time_line_post():
     return {
         "timeline_posts": [
             model_to_dict(p)
-            for p in 
+            for p in
             TimelinePost.select().order_by(TimelinePost.created_at.desc())
         ]
     }
